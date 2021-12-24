8cc04ab37d88659d508f0080276c05215fe647df
David Reaver <johndreaver@gmail.com>
2021-12-24 09:24:13 -0800

    Bazel boilerplate

---
+++ WORKSPACE
@@ -0,0 +1,1 @@
+workspace(name = "bazel_terraform_demo")
---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  inputs = {
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs-unstable }:
+    let
+      pkgs = import nixpkgs-unstable { system = "x86_64-linux"; config = { allowUnfree = true; }; };
+    in {
+      devShell.x86_64-linux = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          bazel
+        ];
+      };
+    };
+}
