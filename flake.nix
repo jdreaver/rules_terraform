a09ed15ac3c37d4ec192cfc262440eb41165928d
David Reaver <johndreaver@gmail.com>
2021-12-31 07:50:08 -0800

    Install graphiviz from nixpkgs

--- flake.nix
+++ flake.nix
@@ -11,6 +11,7 @@
         buildInputs = with pkgs; [
           bazel
           jdk11 # Needed to run some bazel commands
+          graphviz # To visualize bazel graph output
         ];
       };
     };
